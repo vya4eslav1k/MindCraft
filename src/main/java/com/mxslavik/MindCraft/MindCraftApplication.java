7307fb8922c205f69e8a730194f9809fd9890e30
vya4eslav1k <krosh.das@bk.ru>
2024-12-31 20:48:35 +0300

    initial commit

---
+++ src/main/java/com/mxslavik/MindCraft/MindCraftApplication.java
@@ -0,0 +1,13 @@
+package com.mxslavik.MindCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindCraftApplication.class, args);
+	}
+
+}
